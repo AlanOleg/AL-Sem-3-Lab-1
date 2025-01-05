d38dbd65613477bcab0a507f6bb1c1077ab2eb79
Alan <57073942+AlanOleg@users.noreply.github.com>
2025-01-05 17:36:04 +0300

    Add files via upload

---
+++ Task 1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Min and max values of predefined data types in C#:");
+        Console.WriteLine($"int: [{int.MinValue}, {int.MaxValue}]");
+        Console.WriteLine($"uint: [0, {uint.MaxValue}]");
+        Console.WriteLine($"short: [{short.MinValue}, {short.MaxValue}]");
+        Console.WriteLine($"ushort: [0, {ushort.MaxValue}]");
+        Console.WriteLine($"long: [{long.MinValue}, {long.MaxValue}]");
+        Console.WriteLine($"ulong: [0, {ulong.MaxValue}]");
+        Console.WriteLine($"byte: [{byte.MinValue}, {byte.MaxValue}]");
+        Console.WriteLine($"sbyte: [{sbyte.MinValue}, {sbyte.MaxValue}]");
+        Console.WriteLine($"float: [{float.MinValue}, {float.MaxValue}]");
+        Console.WriteLine($"double: [{double.MinValue}, {double.MaxValue}]");
+        Console.WriteLine($"decimal: [{decimal.MinValue}, {decimal.MaxValue}]");
+    }
+}
