4f92824d1f127792540c2be51267ce1328033d87
Alan <57073942+AlanOleg@users.noreply.github.com>
2025-01-05 18:58:47 +0300

    Add files via upload

---
+++ Task 2/AL-Sem-3-Lab-1-task-2-tests.cs
@@ -0,0 +1,34 @@
+﻿using System.Drawing;
+using NUnit.Framework;
+
+[TestFixture]
+public class RectangleTests
+{
+    [Test]
+    public void TestAreaCalculation()
+    {
+        Rectangle rectangle = new Rectangle(5, 10);
+        Assert.AreEqual(50, rectangle.Area, "Площадь должна быть 50 для сторон 5 и 10.");
+    }
+
+    [Test]
+    public void TestPerimeterCalculation()
+    {
+        Rectangle rectangle = new Rectangle(5, 10);
+        Assert.AreEqual(30, rectangle.Perimeter, "Периметр должен быть 30 для сторон 5 и 10.");
+    }
+
+    [Test]
+    public void TestAreaWithDifferentSides()
+    {
+        Rectangle rectangle = new Rectangle(4.5, 6.5);
+        Assert.AreEqual(29.25, rectangle.Area, 0.01, "Площадь должна быть 29.25 для сторон 4.5 и 6.5.");
+    }
+
+    [Test]
+    public void TestPerimeterWithDifferentSides()
+    {
+        Rectangle rectangle = new Rectangle(4.5, 6.5);
+        Assert.AreEqual(22, rectangle.Perimeter, 0.01, "Периметр должен быть 22 для сторон 4.5 и 6.5.");
+    }
+}
---
+++ Task 2/AL-Sem-3-Lab-1-task-2.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+public class Rectangle
+{
+    private double sideA;
+    private double sideB;
+
+    public Rectangle(double sideA, double sideB)
+    {
+        this.sideA = sideA;
+        this.sideB = sideB;
+    }
+
+    private double CalculateArea()
+    {
+        return sideA * sideB;
+    }
+
+    private double CalculatePerimeter()
+    {
+        return 2 * (sideA + sideB);
+    }
+
+    public double Area
+    {
+        get { return CalculateArea(); }
+    }
+
+    public double Perimeter
+    {
+        get { return CalculatePerimeter(); }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите длину стороны A:");
+        double sideA = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите длину стороны B:");
+        double sideB = Convert.ToDouble(Console.ReadLine());
+
+        Rectangle rectangle = new Rectangle(sideA, sideB);
+
+        Console.WriteLine($"Площадь прямоугольника: {rectangle.Area}");
+        Console.WriteLine($"Периметр прямоугольника: {rectangle.Perimeter}");
+    }
+}
